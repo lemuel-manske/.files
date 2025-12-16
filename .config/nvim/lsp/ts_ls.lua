5a67db7a728a9431d81022d6ed3b7331637b5ec2
lemuel-manske <lemuelkauemanskede.liz@philips.com>
2025-12-16 07:31:33 -0300

    Add configuration for typescript-language-server

---
+++ .config/nvim/lsp/ts_ls.lua
@@ -0,0 +1,20 @@
+return {
+  cmd = {
+    "typescript-language-server", "--stdio"
+  },
+
+  filetypes = {
+    "javascript",
+    "javascriptreact",
+    "javascript.jsx",
+    "typescript",
+    "typescriptreact",
+    "typescript.tsx",
+  },
+
+  root_markers = { "tsconfig.json", "jsconfig.json", "package.json", ".git" },
+
+  single_file_support = true,
+
+  log_level = vim.lsp.protocol.MessageType.Warning,
+}
