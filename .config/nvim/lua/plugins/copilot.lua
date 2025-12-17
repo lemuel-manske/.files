4919fd1c0af389f132dc802fc26492e6cb9b03ab
lemuel-manske <lemuelkauemanskede.liz@philips.com>
2025-12-17 15:55:59 -0300

    Use latest node with Copilot and disable Copilot by default

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,3 +1,12 @@
 return {
   "github/copilot.vim",
+
+  init = function()
+    vim.g.copilot_enabled = false
+
+    -- always use nvm's latest node version
+    local node = vim.fn.system("bash -lc 'nvm which node'"):gsub("%s+$", "")
+
+    vim.g.copilot_node_command = node
+  end
 }
