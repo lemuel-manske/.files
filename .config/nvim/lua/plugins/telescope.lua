3bf8ff5e112c0fc552677df93a1045551e76b547
lemuel-manske <lemuelkauemanskede.liz@philips.com>
2026-01-07 11:18:41 -0300

    Pin copilot version

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -5,8 +5,9 @@ return {
 
   dependencies = {
     "nvim-lua/plenary.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     "nvim-tree/nvim-web-devicons",
+
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
   },
 
   config = function()
